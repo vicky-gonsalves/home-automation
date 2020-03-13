62660b35e87cd8b98591c52014ce64d942d84a9c
vicky <vgonsalves@arcfix.com>
2020-03-13 14:52:12 +0530

    Added dynamic env vars

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 const config = {
-  apiUrl: 'http://localhost:9000/v1',
-  socketUrl: 'http://localhost:9000',
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.REACT_APP_PORT,
+  apiUrl: process.env.REACT_APP_API_URL,
+  socketUrl: process.env.REACT_APP_SOCKET_URL,
   appName: 'Home Automation',
 };
 
