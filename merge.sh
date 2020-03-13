153437cbf330538ea0d486456ab86c6fa360670d
vicky <vgonsalves@arcfix.com>
2020-03-13 16:48:14 +0530

    Added .env.example and .env.production and updated package.json

--- merge.sh
+++ merge.sh
@@ -10,6 +10,7 @@ git reset --hard
 git checkout master
 git merge --ff-only "$TRAVIS_COMMIT"
 git push git+ssh://git@github.com/${TRAVIS_REPO_SLUG}.git master
+echo "Merging development into master"
 else
 echo "Skipping merge into master because branch is not 'development'"
 fi
