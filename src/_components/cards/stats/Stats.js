c987344b0ab1851a4e46511c84ec7d5ca5cc4cf7
vicky <vgonsalves@arcfix.com>
2020-04-08 19:52:08 +0530

    added unit test for Stats.js and optimized

--- src/_components/cards/stats/Stats.js
+++ src/_components/cards/stats/Stats.js
@@ -28,38 +28,63 @@ const useStyles = makeStyles(theme => ({
     cursor: 'pointer',
   },
 }));
-const Stats = props => {
+const Stats = ({ deviceId }) => {
   const classes = useStyles();
   const allLogs = useSelector(state => state.log && state.log.logs);
-  const thisLogs = allLogs.filter(log => log.deviceId === props.deviceId);
+  const thisLogs = allLogs.filter(log => log.deviceId === deviceId);
+
+  const renderStatsAlert = () => {
+    if (thisLogs.length <= 0) {
+      return (
+        <Alert severity="info" data-test="StatsAlertComponent">
+          It seems there are no logs yet.
+        </Alert>
+      );
+    }
+  };
+
+  const renderDeviceLog = log => {
+    if (log && log.triggeredByDevice) {
+      return (
+        <ListItemIcon className={classes.cursor} data-test="StatsListItemDeviceIconComponent">
+          <Tooltip title="Action performed automatically by system">
+            <AdbIcon />
+          </Tooltip>
+        </ListItemIcon>
+      );
+    }
+  };
+
+  const renderManualLog = log => {
+    if (!log.triggeredByDevice) {
+      return (
+        <ListItemIcon className={classes.cursor} data-test="StatsListItemManualIconComponent">
+          <Tooltip title="Action performed manually by user">
+            <PermIdentityIcon />
+          </Tooltip>
+        </ListItemIcon>
+      );
+    }
+  };
+
+  const renderLogList = (log, index) => {
+    return (
+      <ListItem key={`${deviceId}-log-${index}`} data-test="StatsListItemComponent">
+        {renderDeviceLog(log)}
+        {renderManualLog(log)}
+        <Typography key={log.id} component="div" color="textPrimary" variant="caption">
+          {log.logDescription} - by {log.triggeredByDevice ? 'system' : log.userName} <br /> -{' '}
+          {moment(log.createdAt).format('MMMM Do YYYY, h:mm:ss A')}
+        </Typography>
+      </ListItem>
+    );
+  };
+
   return (
-    <CardContent className={classes.cardContent}>
-      {thisLogs.length <= 0 && <Alert severity="info">It seems there are no logs yet.</Alert>}
+    <CardContent className={classes.cardContent} data-test="StatsCardContainer">
+      {renderStatsAlert()}
       <List className={classes.root} subheader={<li />}>
-        {thisLogs &&
-          thisLogs.length > 0 &&
-          thisLogs.map((log, index) => (
-            <ListItem key={`${props.deviceId}-log-${index}`}>
-              {log.triggeredByDevice && (
-                <ListItemIcon className={classes.cursor}>
-                  <Tooltip title="Action performed automatically by system">
-                    <AdbIcon />
-                  </Tooltip>
-                </ListItemIcon>
-              )}
-              {!log.triggeredByDevice && (
-                <ListItemIcon className={classes.cursor}>
-                  <Tooltip title="Action performed manually by user">
-                    <PermIdentityIcon />
-                  </Tooltip>
-                </ListItemIcon>
-              )}
-              <Typography key={log.id} component="div" color="textPrimary" variant="caption">
-                {log.logDescription} - by {log.triggeredByDevice ? 'system' : log.userName} <br /> -{' '}
-                {moment(log.createdAt).format('MMMM Do YYYY, h:mm:ss A')}
-              </Typography>
-            </ListItem>
-          ))}
+        {thisLogs && thisLogs.length > 0 && thisLogs.map((log, index) => renderLogList(log, index))}
       </List>
     </CardContent>
   );
---
+++ src/_components/cards/stats/Stats.test.js
@@ -0,0 +1,97 @@
+import { mount } from 'enzyme';
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { findByDataAttr, initialState } from '../../../_utils';
+import { deviceOne } from '../../../_utils/fixtures/device.fixture';
+import { logFive, logFour, logOne, logSix, logThree, logTwo } from '../../../_utils/fixtures/log.fixture';
+import Stats from './Stats';
+
+let store;
+const props = {
+  deviceId: deviceOne.deviceId,
+};
+const mockStore = configureStore([thunk]);
+const setupWrapper = _initialState => {
+  store = mockStore(_initialState);
+  return mount(
+    <Provider store={store}>
+      <Stats {...props} />
+    </Provider>
+  );
+};
+
+describe('Stats', () => {
+  describe('Components Testing with state', () => {
+    let wrapper;
+    beforeEach(() => {});
+
+    afterEach(() => {
+      wrapper.unmount();
+      store.clearActions();
+    });
+
+    it('should render without error', () => {
+      wrapper = setupWrapper(initialState);
+      const component = findByDataAttr(wrapper, 'StatsCardContainer').first();
+      expect(component.length).toBe(1);
+    });
+
+    it('should render StatsAlertComponent if no state', () => {
+      wrapper = setupWrapper(initialState);
+      const component = findByDataAttr(wrapper, 'StatsAlertComponent').first();
+      expect(component.length).toBe(1);
+    });
+
+    it('should not render StatsListItemComponent if no logs', () => {
+      wrapper = setupWrapper(initialState);
+      const component = findByDataAttr(wrapper, 'StatsListItemComponent').first();
+      expect(component.length).toBe(0);
+    });
+
+    it('should not render StatsListItemDeviceIconComponent if no logs', () => {
+      wrapper = setupWrapper(initialState);
+      const component = findByDataAttr(wrapper, 'StatsListItemDeviceIconComponent').first();
+      expect(component.length).toBe(0);
+    });
+
+    it('should not render StatsListItemManualIconComponent if no logs', () => {
+      wrapper = setupWrapper(initialState);
+      const component = findByDataAttr(wrapper, 'StatsListItemManualIconComponent').first();
+      expect(component.length).toBe(0);
+    });
+
+    it('should not render StatsListItemDeviceIconComponent if has logs but not triggered by device', () => {
+      const _initialState = initialState;
+      _initialState.log.logs = [logOne, logTwo, logThree, logFour];
+      wrapper = setupWrapper(_initialState);
+      const component = findByDataAttr(wrapper, 'StatsListItemDeviceIconComponent').first();
+      expect(component.length).toBe(0);
+    });
+
+    it('should not render StatsListItemManualIconComponent if has logs but not triggered by user', () => {
+      const _initialState = initialState;
+      _initialState.log.logs = [logFive, logSix];
+      wrapper = setupWrapper(_initialState);
+      const component = findByDataAttr(wrapper, 'StatsListItemManualIconComponent').first();
+      expect(component.length).toBe(0);
+    });
+
+    it('should render StatsListItemDeviceIconComponent if has logs and triggered by device', () => {
+      const _initialState = initialState;
+      _initialState.log.logs = [logFive, logSix];
+      wrapper = setupWrapper(_initialState);
+      const component = findByDataAttr(wrapper, 'StatsListItemDeviceIconComponent').first();
+      expect(component.length).toBe(1);
+    });
+
+    it('should not render StatsListItemManualIconComponent if has logs but not triggered by user', () => {
+      const _initialState = initialState;
+      _initialState.log.logs = [logOne, logTwo, logThree, logFour];
+      wrapper = setupWrapper(_initialState);
+      const component = findByDataAttr(wrapper, 'StatsListItemManualIconComponent').first();
+      expect(component.length).toBe(1);
+    });
+  });
+});
