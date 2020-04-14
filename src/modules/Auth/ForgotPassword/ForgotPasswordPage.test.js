5ddcd59fc1fa30132bb4646779d4e4f123ef23f7
vicky <vgonsalves@arcfix.com>
2020-04-14 11:44:22 +0530

    Added tests for ForgotPasswordPage.js

---
+++ src/modules/Auth/ForgotPassword/ForgotPasswordPage.test.js
@@ -0,0 +1,34 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import { history } from '../../../_helpers/history';
+import { checkProps, findByDataAttr } from '../../../_utils';
+import ForgotPasswordPage from './ForgotPasswordPage';
+
+const props = {
+  history,
+  location: {},
+  match: {},
+};
+
+describe('NotFound Page', () => {
+  describe('Checking PropTypes', () => {
+    it('should not throw a warning', () => {
+      const propsErr = checkProps(ForgotPasswordPage, props);
+      expect(propsErr).toBeUndefined();
+    });
+  });
+
+  describe('Components Testing', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = shallow(<ForgotPasswordPage {...props} />);
+    });
+
+    afterEach(() => {});
+
+    it('should render without error', () => {
+      const component = findByDataAttr(wrapper, 'forgotPasswordPageContainer').first();
+      expect(component.length).toBe(1);
+    });
+  });
+});
