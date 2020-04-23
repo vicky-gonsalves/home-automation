501a2af7f722bc282af6f3079b66959562cf244e
vicky <vgonsalves@arcfix.com>
2020-04-23 12:26:02 +0530

    Converted DashboardPage.js to functional component

--- src/modules/Admin/Dashboard/DashboardPage.js
+++ src/modules/Admin/Dashboard/DashboardPage.js
@@ -1,14 +1,14 @@
-import { CssBaseline, withStyles } from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import AdminDrawer from '../../../_components/admin-drawer/adminDrawer';
 import Footer from '../../../_components/footer';
 import Navbar from '../../../_components/navbar/navbar';
 import config from '../../../config';
 
-const useStyles = theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     padding: theme.spacing(2),
@@ -17,26 +17,24 @@ const useStyles = theme => ({
     padding: theme.spacing(3, 2),
     marginTop: 'auto',
   },
-});
+}));
 
-export class DashboardPage extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <React.Fragment>
-        <CssBaseline />
-        <Navbar appName={config.appName} data-test="navbarComponent" />
-        <AdminDrawer data-test="adminDrawerComponent" />
-        <Container disableGutters={true} maxWidth="xl" data-test="dashboardPageContainer">
-          <div className={classes.root}>Admin Page</div>
-        </Container>
-        <footer className={classes.footer}>
-          <Footer appName={config.appName} data-test="footerComponent" />
-        </footer>
-      </React.Fragment>
-    );
-  }
-}
+const DashboardPage = () => {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Navbar appName={config.appName} data-test="navbarComponent" />
+      <AdminDrawer data-test="adminDrawerComponent" />
+      <Container disableGutters={true} maxWidth="xl" data-test="dashboardPageContainer">
+        <div className={classes.root}>Admin Page</div>
+      </Container>
+      <footer className={classes.footer}>
+        <Footer appName={config.appName} data-test="footerComponent" />
+      </footer>
+    </React.Fragment>
+  );
+};
 
 DashboardPage.propTypes = {
   classes: PropTypes.shape({
@@ -48,12 +46,4 @@ DashboardPage.propTypes = {
   match: PropTypes.object.isRequired,
 };
 
-function mapState() {
-  return {};
-}
-
-const actionCreators = {};
-
-const connectedDashboardPage = connect(mapState, actionCreators)(DashboardPage);
-
-export default withStyles(useStyles)(connectedDashboardPage);
+export default DashboardPage;
