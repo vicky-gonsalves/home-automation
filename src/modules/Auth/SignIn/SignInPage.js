ee72a6b188a898d46f8680ac0350c557d1078339
vicky <vgonsalves@arcfix.com>
2020-04-23 12:17:23 +0530

    Converted SignInPage.js to functional component

--- src/modules/Auth/SignIn/SignInPage.js
+++ src/modules/Auth/SignIn/SignInPage.js
@@ -1,21 +1,21 @@
-import { CssBaseline, withStyles } from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { userActions } from '../../../_actions/user/user.actions';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Footer from '../../../_components/footer/footer';
 import SignInForm from '../../../_components/forms/signIn-form/SignInForm';
 import Navbar from '../../../_components/navbar/navbar';
 import { history } from '../../../_helpers/history/history';
 import config from '../../../config';
 
-const useStyles = theme => ({
+const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
@@ -35,68 +35,55 @@ const useStyles = theme => ({
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
-});
+}));
 
-export class SignInPage extends Component {
-  constructor(props) {
-    super(props);
-    this.isLoggedIn = this.props.isLoggedIn && this.props.tokens !== null;
-    if (this.isLoggedIn) history.push('/');
-  }
+const SignInPage = () => {
+  const classes = useStyles();
+  const currentUser = useSelector(state => state.user);
+  const isLoggedIn = currentUser.isLoggedIn && currentUser.tokens !== null;
 
-  // eslint-disable-next-line class-methods-use-this
-  navigateToForgotPass() {
-    history.push('/forgot-password');
-  }
+  const navigateTo = path => {
+    history.push(path);
+  };
 
-  componentDidUpdate() {
-    this.isLoggedIn = this.props.isLoggedIn && this.props.tokens !== null;
-    if (this.isLoggedIn) history.push('/home');
-  }
+  useEffect(() => {
+    const navigateToHome = () => {
+      if (isLoggedIn) {
+        navigateTo('/home');
+      }
+    };
+    navigateToHome();
+  }, [isLoggedIn]);
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <React.Fragment>
-        <CssBaseline />
-        <Navbar appName={config.appName} data-test="navbarComponent" />
-        <Container component="main" maxWidth="xs" data-test="signInContainer">
-          <div className={classes.paper}>
-            <Avatar className={classes.avatar}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <div data-test="signInFormComponent">
-              <SignInForm />
-            </div>
-            <Grid container>
-              <Grid item xs>
-                <Button color="primary" size="small" onClick={this.navigateToForgotPass} data-test="forgotPassword">
-                  Forgot password?
-                </Button>
-              </Grid>
-            </Grid>
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Navbar appName={config.appName} data-test="navbarComponent" />
+      <Container component="main" maxWidth="xs" data-test="signInContainer">
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <div data-test="signInFormComponent">
+            <SignInForm />
           </div>
-        </Container>
-        {/* Footer */}
-        <footer className={classes.footer}>
-          <Footer appName={config.appName} data-test="footerComponent" />
-        </footer>
-        {/* End footer */}
-      </React.Fragment>
-    );
-  }
-}
-
-function mapState(state) {
-  const { isLoggedIn, tokens } = state.user;
-  return { isLoggedIn, tokens };
-}
-
-const actionCreators = {
-  signIn: userActions.signIn,
+          <Grid container>
+            <Grid item xs>
+              <Button color="primary" size="small" onClick={() => navigateTo('/forgot-password')} data-test="forgotPassword">
+                Forgot password?
+              </Button>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+      <footer className={classes.footer}>
+        <Footer appName={config.appName} data-test="footerComponent" />
+      </footer>
+    </React.Fragment>
+  );
 };
 
 SignInPage.propTypes = {
@@ -109,11 +96,6 @@ SignInPage.propTypes = {
   history: PropTypes.object.isRequired,
   location: PropTypes.object.isRequired,
   match: PropTypes.object.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired,
-  tokens: PropTypes.object,
-  signIn: PropTypes.func.isRequired,
 };
 
-const connectedSignInPage = connect(mapState, actionCreators)(SignInPage);
-
-export default withStyles(useStyles)(connectedSignInPage);
+export default SignInPage;
