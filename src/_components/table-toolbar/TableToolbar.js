66cfeeffecb5b0e242a1e3970eb05de77b83e4f7
vicky <vgonsalves@arcfix.com>
2020-04-27 19:36:30 +0530

    Added tests for TableToolbar.js

--- src/_components/table-toolbar/TableToolbar.js
+++ src/_components/table-toolbar/TableToolbar.js
@@ -21,24 +21,30 @@ const useStyles = makeStyles(theme => ({
 const TableToolbar = ({ title, buttons }) => {
   const classes = useStyles();
 
-  const renderButtons = () => {
-    if (buttons && buttons.length) {
-      return buttons.map((button, index) => (
-        <div key={index}>
-          <button.component path={button.path} title={button.title} />
-        </div>
-      ));
+  const renderButton = button => {
+    if (button.component) {
+      return <button.component path={button.path} title={button.title} data-test="toolbarButtonComponent" />;
     }
+    return null;
   };
 
-  return (
-    <Toolbar className={classes.toolbar}>
-      <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
-        {title}
-      </Typography>
-      {renderButtons()}
-    </Toolbar>
-  );
+  const renderButtons = () => buttons.map((button, index) => <div key={index}>{renderButton(button)}</div>);
+
+  const renderButtonsContainer = () => {
+    if (buttons && buttons.length) {
+      return (
+        <Toolbar className={classes.toolbar} data-test="toolbarContainer">
+          <Typography className={classes.title} variant="h6" id="tableTitle" component="div" data-test="tableTitleComponent">
+            {title}
+          </Typography>
+          {renderButtons()}
+        </Toolbar>
+      );
+    }
+    return null;
+  };
+
+  return renderButtonsContainer();
 };
 
 TableToolbar.propTypes = {
@@ -51,8 +57,10 @@ TableToolbar.propTypes = {
   buttons: PropTypes.arrayOf(
     PropTypes.shape({
       component: PropTypes.func.isRequired,
+      type: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
       buttonType: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
     })
   ),
 };
