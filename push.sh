b830a49beef6c0315659fdc4a8b0aa993d17a4db
vicky <vgonsalves@arcfix.com>
2020-06-14 12:18:36 +0530

    Added push script

--- push.sh
+++ push.sh
@@ -1,28 +1,40 @@
 #!/bin/sh
 
 setup_git() {
+  echo "setting up git"
   git config --global user.email "vicky.gonsalves@outlook.com"
   git config --global user.name "Vicky Gonsalves"
 }
 
 clone_repo(){
+  echo "cloning repo"
   cd ..
   git clone https://$GIT_PERSONAL_ACCESS_TOKEN:x-oauth-basic@github.com/vicky-gonsalves/build.git
 }
 
 move_build(){
+  echo "moving build"
   cd build
   rm -R build
   mv ../home-automation/build build
 }
 
 commit_website_files() {
+  echo "commiting build"
   git add -A
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
+  echo "pushing build"
   git push origin master
+}
+
+cleanup() {
+  echo "cleaning up"
+  cd ..
+  rm -R build
+  rm -R home-automation
 }
 
 setup_git
@@ -30,3 +42,4 @@ clone_repo
 move_build
 commit_website_files
 upload_files
+cleanup
